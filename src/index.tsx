ac345702e4c59d73557c06eb10842d451416348a
Mainendra <1897340+mainendra@users.noreply.github.com>
2023-09-11 21:02:55 -0400

    Updated page title

--- src/index.tsx
+++ src/index.tsx
@@ -74,7 +74,7 @@ const BaseHtml = ({ children }: PropsWithChildren) => `
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>THE BETH STACK</title>
+<title>View Transitions</title>
 <script src="https://unpkg.com/htmx.org@1.9.3"></script>
 <script src="https://unpkg.com/hyperscript.org@0.9.9"></script>
 <script> htmx.config.globalViewTransitions = true; </script>
